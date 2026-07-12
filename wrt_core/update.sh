53d453d90c5c01ce44a056a1a62567d8888e2972
langcunfu <55777476+langcunfu@users.noreply.github.com>
2026-07-13 01:30:50 +0800

    Update update.sh
    Signed-off-by: langcunfu <55777476+langcunfu@users.noreply.github.com>

--- wrt_core/update.sh
+++ wrt_core/update.sh
@@ -83,10 +83,10 @@ stage_pre_install_source_fixes() {
     update_ath11k_fw
     # fix_mkpkg_format_invalid
     change_cpuusage
-    update_tcping
+#    update_tcping
     add_ax6600_led
     set_custom_task
-    apply_passwall_tweaks
+#    apply_passwall_tweaks
     update_nss_pbuf_performance
     set_build_signature
     update_nss_diag
