8b0a250cd2eb298eae7c1e45e0906328822a9070
langcunfu <55777476+langcunfu@users.noreply.github.com>
2026-07-14 17:36:30 +0800

    Update custom_feed.sh
    Signed-off-by: langcunfu <55777476+langcunfu@users.noreply.github.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -196,7 +196,7 @@ install_custom_feed() {
         "Openwrt-Passwall/openwrt-passwall|https://github.com/Openwrt-Passwall/openwrt-passwall.git|main|luci-app-passwall"
         "nikkinikki-org/OpenWrt-nikki|https://github.com/nikkinikki-org/OpenWrt-nikki.git|main|nikki luci-app-nikki mihomo-meta"
         # 新增易有云后端主程序仓库 nas 分支master
-        "linkease/nas-packages|https://github.com/linkease/nas-packages.git|master|network/services/linkease"
+        "linkease/nas-packages|https://github.com/linkease/nas-packages.git|master|network/services/linkease network/services/linkmount"
         # 新增易有云LuCI界面仓库 nas\_luci 分支main
         "linkease/nas-packages-luci|https://github.com/linkease/nas-packages-luci.git|main|luci/luci-app-linkease"
     )
