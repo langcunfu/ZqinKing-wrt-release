3b9c91dff8247a14b556c71fcb7fe99993d1ef05
langcunfu <55777476+langcunfu@users.noreply.github.com>
2026-08-16 02:22:42 +0800

    Update custom_feed.sh
    Signed-off-by: langcunfu <55777476+langcunfu@users.noreply.github.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -164,7 +164,7 @@ install_custom_feed() {
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
         lucky luci-app-lucky luci-app-openclash luci-app-homeproxy luci-app-amlogic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd luci-app-linkease
+        msd_lite luci-app-msd_lite cups luci-app-cupsd linkease luci-app-linkease
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
